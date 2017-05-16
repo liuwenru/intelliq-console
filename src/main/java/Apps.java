8727d763fa3b5b91e1b72349c1d5d6517fcda7c0
ijarvis <ijarvis@sina.com>
2017-05-17 00:27:47 +0800

    add mvn support

---
+++ src/main/java/Apps.java
@@ -0,0 +1,17 @@
+/**
+ * Created by ijarvis on 2017/5/17.
+ */
+public class Apps {
+
+    public void printMesg(){
+        System.out.println("Success.........");
+    }
+
+    public static void  main(String[] args){
+        System.out.println("print main process.........");
+    }
+
+
+
+
+}
---
+++ src/test/java/AppTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * Created by ijarvis on 2017/5/17.
+ */
+public class AppTest {
+    @Test
+    public void testPrintMessage(){
+        Apps tmp=new Apps();
+        tmp.printMesg();
+    }
+}
