5c458c38b891412c1676c297142acfb79a814dca
ijarvis <ijarvis@sina.com>
2017-06-12 16:51:56 +0800

    add get and set method support

--- src/main/java/RedisTest/RedisTestUtil.java
+++ src/main/java/RedisTest/RedisTestUtil.java
@@ -4,20 +4,25 @@ import org.apache.log4j.Logger;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
-
 import java.util.UUID;
 
 public class RedisTestUtil {
     private static Logger logger=Logger.getLogger(RedisTestUtil.class);
-    public static void doPerTest(){
+    private static Jedis jedis=null;
+    private static int COUNT=10000;
+    public static void init(){
         JedisPoolConfig config = new JedisPoolConfig();
         config.setMaxTotal(1000);
         config.setMaxIdle(100);
         config.setTestOnBorrow(true);
         JedisPool pool = new JedisPool(config,"192.168.206.247",6379);
-        Jedis jedis=pool.getResource();
+        jedis=pool.getResource();
+    }
+    public static void doSetPer(){
+        if (RedisTestUtil.jedis==null){
+            RedisTestUtil.init();
+        }
         long start=System.currentTimeMillis();
-        int COUNT=100000;
         for (int i =0;i<COUNT;i++) {
             if ((COUNT%10000)==0){
                 logger.debug("Success.........."+(i*1.0/COUNT)*100+" %");
@@ -27,28 +32,24 @@ public class RedisTestUtil {
             jedis.set(key,value.toString());
         }
         long end=System.currentTimeMillis();
-        logger.debug("----------------------------------------------------------");
-        logger.debug("set "+ COUNT+" keys"+(end-start) );
-        logger.debug("----------------------------------------------------------");
+        logger.info("-------------------------SET---------------------------------");
+        logger.info("set "+ COUNT+" keys"+(end-start) );
+        logger.info("-------------------------------------------------------------");
     }
     public static void doGetPer(){
-        JedisPoolConfig config = new JedisPoolConfig();
-        config.setMaxTotal(1000);
-        config.setMaxIdle(100);
-        config.setTestOnBorrow(true);
-        JedisPool pool = new JedisPool(config,"192.168.206.247",6379);
-        Jedis jedis=pool.getResource();
+        if (RedisTestUtil.jedis==null){
+            RedisTestUtil.init();
+        }
         long start=System.currentTimeMillis();
-        int COUNT=100000;
         for (int i =0;i<COUNT;i++) {
-            if ((COUNT%10000)==0){
+            if ((COUNT%1000)==0){
                 logger.debug("Success.........."+(i*1.0/COUNT)*100+" %");
             }
             jedis.get(Integer.toString(i));
         }
         long end=System.currentTimeMillis();
-        logger.debug("----------------------------------------------------------");
-        logger.debug("set "+ COUNT+" keys"+(end-start) );
-        logger.debug("----------------------------------------------------------");
+        logger.info("------------------------GET------------------------------------");
+        logger.info("set "+ COUNT+" keys"+(end-start) );
+        logger.info("----------------------------------------------------------------");
     }
 }
