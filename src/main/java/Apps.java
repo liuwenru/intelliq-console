d41da589df47dd501fc8c0ee94d6aaca2a6d1123
ijarvis <ijarvis@sina.com>
2017-08-01 20:27:40 +0800

    use the default test

--- src/main/java/Apps.java
+++ src/main/java/Apps.java
@@ -1,4 +1,9 @@
-import redis.clients.jedis.Jedis;
+
+
+import org.apache.log4j.Logger;
+import org.csource.common.MyException;
+import org.csource.common.NameValuePair;
+import org.csource.fastdfs.*;
 
 import java.io.*;
 
@@ -6,22 +11,36 @@ import java.io.*;
  * Created by ijarvis on 2017/5/17.
  */
 public class Apps {
+    public static Logger logger=Logger.getLogger(Apps.class);
+    public static void  main(String[] args) throws IOException, MyException {
 
 
-    public static void  main(String[] args) throws IOException {
-        try {
-            Jedis jedis = new Jedis(args[0],Integer.parseInt(args[1]));
-            if (args.length==3 && !args[2].equals("")){
-                jedis.auth(args[2]);
-            }
-            jedis.set("epoint","success");
-            System.out.println("set key success。。。。。。。。。。。。。");
-            jedis.close();
-        }catch (Exception e) {
-            throw e;
+
+
+        //"/Users/ijarvis/IdeaProjects/intelliq-console/src/main/resources/fdfs_client.conf"
+        String conf_filename=args[0];
+        String local_filename="/Users/ijarvis/IdeaProjects/intelliq-console/src/main/resources/images/aliyun120.jpg";
+        ClientGlobal.init(conf_filename);
+        TrackerClient tracker = new TrackerClient();
+        TrackerServer trackerServer = tracker.getConnection();
+        StorageServer storageServer = null;
+        StorageClient storageClient = new StorageClient(trackerServer, storageServer);
+        //String fileIds[] = storageClient.upload_file(local_filename, "png", null);
+        //byte[] ArrayLetters = {0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69, 0x6A, 0x6B, 0x6C, 0x6D, 0x6E, 0x6F, 0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77, 0x78, 0x79, 0x7A};
+        byte probyte=0x61;
+        byte[] bytes=new byte[Integer.parseInt(args[1])*1024];
+        for (int i=0;i< Integer.parseInt(args[1])*1024;i++){
+            bytes[i]=probyte;
+        }
+        int j=0;
+        while (true){
+            long start=System.currentTimeMillis();
+            String[] fileid=storageClient.upload_file(bytes,"txt",null);
+            logger.debug("finish "+(j++)+" push files "+fileid[0]+"/"+fileid[1]+" use :"+(System.currentTimeMillis()-start));
         }
 
+        //System.out.println(fileIds.length);
+        //System.out.println("组名：" + fileIds[0]);
+        //System.out.println("路径: " + fileIds[1]);
     }
-
-
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,13 +1,69 @@
+import org.apache.log4j.Logger;
+import org.csource.common.MyException;
+import org.csource.common.NameValuePair;
+import org.csource.fastdfs.*;
 import org.junit.Ignore;
 import org.junit.Test;
+
+import java.io.*;
+import java.net.InetSocketAddress;
 
 /**
  * Created by ijarvis on 2017/5/17.
  */
 public class AppTest {
+    public static Logger logger=Logger.getLogger(AppTest.class);
+    String conf_filename="/Users/ijarvis/IdeaProjects/intelliq-console/src/main/resources/fdfs_client.conf";
+    String local_filename="/Users/ijarvis/IdeaProjects/intelliq-console/src/main/resources/images/images1.jpg";
     @Ignore
     @Test
-    public void testPrintMessage(){
-        Apps tmp=new Apps();
+    public void TestUploadFiles() throws IOException, MyException {
+        ClientGlobal.init(conf_filename);
+        TrackerClient tracker = new TrackerClient();
+        TrackerServer trackerServer = tracker.getConnection();
+        StorageServer storageServer = null;
+        StorageClient storageClient = new StorageClient(trackerServer, storageServer);
+        String fileIds[] = storageClient.upload_file(local_filename, "png", null);
+        logger.debug(fileIds.length);
+        logger.debug("组名：" + fileIds[0]);
+        logger.debug("路径: " + fileIds[0]+"/"+fileIds[1]);
+    }
+    @Ignore
+    @Test
+    public void TestUploadMethod2() throws IOException, MyException {
+        ClientGlobal.init(conf_filename);
+        TrackerClient tracker = new TrackerClient();
+        TrackerServer trackerServer = tracker.getConnection();
+        StorageServer storageServer = null;
+        StorageClient1 storageClient1 = new StorageClient1(trackerServer, storageServer);
+        String fileIds = storageClient1.upload_file1(local_filename, "png", null);
+        logger.debug("文件ID:"+fileIds);
+        byte[] result = storageClient1.download_file1(fileIds);
+        File file=new File("/Users/ijarvis/Downloads/aa.png");
+        OutputStream fileoutput=new FileOutputStream(file);
+        fileoutput.write(result);
+        fileoutput.close();
+    }
+    @Test
+    public void TestTracker() throws IOException, MyException {
+        //测试一下基本的获取一些关于存储的信息相关
+        InetSocketAddress[] inetSocketAddresses=new InetSocketAddress[1];
+        inetSocketAddresses[0]=new InetSocketAddress("192.168.210.2",22122);
+        //inetSocketAddresses[1]=new InetSocketAddress("192.168.210.3",22122);
+        TrackerGroup tg=new TrackerGroup(inetSocketAddresses);
+        TrackerClient tc=new TrackerClient(tg);
+        TrackerServer ts=tc.getConnection();
+        String groupname=null;
+        StorageServer[] storageServers=tc.getStoreStorages(ts,null);
+        if (storageServers == null) {
+            logger.debug("get store storage servers fail, error code: " + tc.getErrorCode());
+        } else {
+            logger.debug("store storage servers count: " + storageServers.length);
+            for (int k = 0; k < storageServers.length; k++) {
+                logger.debug((k + 1) + ". " + storageServers[k].getInetSocketAddress().getAddress().getHostAddress() + ":" + storageServers[k].getInetSocketAddress().getPort());
+            }
+            logger.debug("");
+        }
+        logger.debug("connection Success...........");
     }
 }
