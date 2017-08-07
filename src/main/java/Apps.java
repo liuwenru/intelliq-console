8ba6c7839789d110e3d35f51cc9c99dddc5d25ee
ijarvis <ijarvis@sina.com>
2017-08-07 11:38:09 +0800

    fix bug

--- src/main/java/Apps.java
+++ src/main/java/Apps.java
@@ -13,10 +13,6 @@ import java.io.*;
 public class Apps {
     public static Logger logger=Logger.getLogger(Apps.class);
     public static void  main(String[] args) throws IOException, MyException {
-
-
-
-
         //"/Users/ijarvis/IdeaProjects/intelliq-console/src/main/resources/fdfs_client.conf"
         String conf_filename=args[0];
         String local_filename="/Users/ijarvis/IdeaProjects/intelliq-console/src/main/resources/images/aliyun120.jpg";
@@ -25,8 +21,6 @@ public class Apps {
         TrackerServer trackerServer = tracker.getConnection();
         StorageServer storageServer = null;
         StorageClient storageClient = new StorageClient(trackerServer, storageServer);
-        //String fileIds[] = storageClient.upload_file(local_filename, "png", null);
-        //byte[] ArrayLetters = {0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69, 0x6A, 0x6B, 0x6C, 0x6D, 0x6E, 0x6F, 0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77, 0x78, 0x79, 0x7A};
         byte probyte=0x61;
         byte[] bytes=new byte[Integer.parseInt(args[1])*1024];
         for (int i=0;i< Integer.parseInt(args[1])*1024;i++){
@@ -34,11 +28,13 @@ public class Apps {
         }
         int j=0;
         while (true){
+
             long start=System.currentTimeMillis();
             String[] fileid=storageClient.upload_file(bytes,"txt",null);
-            logger.debug("finish "+(j++)+" push files "+fileid[0]+"/"+fileid[1]+" use :"+(System.currentTimeMillis()-start));
+            long end=System.currentTimeMillis();
+            logger.debug("upload" + (Integer.parseInt(args[1])*1024)/((end-start)/1.0)/1000+"Mb/s");
+            j++;
         }
-
         //System.out.println(fileIds.length);
         //System.out.println("组名：" + fileIds[0]);
         //System.out.println("路径: " + fileIds[1]);
