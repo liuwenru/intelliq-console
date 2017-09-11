923dbee03c77dcb2ea7228ed63e3ea7b06a809be
ijarvis <ijarvis@sina.com>
2017-09-11 11:00:24 +0800

    add rabbitmq crt

--- src/main/java/Apps.java
+++ src/main/java/Apps.java
@@ -1,34 +1,65 @@
 import org.apache.log4j.Logger;
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
-import redis.clients.jedis.JedisPoolConfig;
 
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.GetResponse;
+
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.*;
+import java.security.cert.CertificateException;
+import java.util.concurrent.TimeoutException;
 
 public class Apps {
     public static Logger logger=Logger.getLogger(Apps.class);
-    public static void  main(String[] args) {
-        try{
-            JedisPool jedisPool;
-            JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
-            String hostip=args[0];
-            int port=Integer.parseInt(args[1]);
-            String password=args[2];
-            jedisPool = new JedisPool(jedisPoolConfig,hostip,port,3000,password,Integer.parseInt(args[3]));
-            Jedis jedis = jedisPool.getResource();
-            int i=1;
-            while (true){
-                jedis.set("epoint", String.valueOf(i));
-                i++;
-                logger.debug("Setting "+i+"     "+ jedis.get("epoint"));
-                Thread.sleep(2000);
-            }
+    public static void  main(String[] args) throws KeyStoreException, IOException, CertificateException, NoSuchAlgorithmException, UnrecoverableKeyException, KeyManagementException, TimeoutException {
+        char[] keyPassphrase = "p@ssw0rd".toCharArray();
+        KeyStore ks = KeyStore.getInstance("PKCS12");
+        ks.load(new FileInputStream("/Users/ijarvis/IdeaProjects/intelliq-console/src/main/resources/rabbitmqssl/rabbitmq-epointclient.keycert.p12"), keyPassphrase);
 
+        KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+        kmf.init(ks, keyPassphrase);
 
-        }catch (Exception e){
-            logger.info("Test FastDFS errot");
-            logger.error("Test FastDFS errot",e);
+        char[] trustPassphrase = "p@ssw0rd".toCharArray();
+        KeyStore tks = KeyStore.getInstance("JKS");
+        tks.load(new FileInputStream("/Users/ijarvis/IdeaProjects/intelliq-console/src/main/resources/rabbitmqssl/rabbitmqstore"), trustPassphrase);
+
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+        tmf.init(tks);
+
+        SSLContext c = SSLContext.getInstance("TLSv1.1");
+        c.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("192.168.209.113");
+        factory.setPort(5671);
+        factory.useSslProtocol(c);
+        factory.setUsername("epoint");
+        factory.setPassword("epoint");
+        Connection conn = factory.newConnection();
+        Channel channel = conn.createChannel();
+
+        channel.queueDeclare("rabbitmq-java-test", false, true, true, null);
+        channel.basicPublish("", "rabbitmq-java-test", null, "Hello, World via SSL".getBytes());
+
+        GetResponse chResponse = channel.basicGet("rabbitmq-java-test", false);
+        if (chResponse == null)
+        {
+            System.out.println("No message retrieved");
+        }
+        else
+        {
+            byte[] body = chResponse.getBody();
+            System.out.println("Recieved: " + new String(body));
         }
 
+        channel.close();
+        conn.close();
 
     }
 }
