d3e2fe1c5b40faaf2141e5bc9a147abcf60414fd
ijarvis <ijarvis@sina.com>
2017-11-06 15:00:53 +0800

    fix bug

--- src/main/java/Apps.java
+++ src/main/java/Apps.java
@@ -10,13 +10,7 @@ public class Apps {
 
     public static void  main(String[] args) throws IOException {
         try {
-            Jedis jedis = new Jedis(args[0],Integer.parseInt(args[1]));
-            if (args.length==3 && !args[2].equals("")){
-                jedis.auth(args[2]);
-            }
-            jedis.set("epoint","success");
             System.out.println("set key success。。。。。。。。。。。。。");
-            jedis.close();
         }catch (Exception e) {
             throw e;
         }
