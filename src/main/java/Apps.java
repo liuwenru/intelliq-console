661225c4dc426a1d799432a6e64111c889701831
ijarvis <ijarvis@sina.com>
2017-12-06 23:06:58 +0800

    fix bug

--- src/main/java/Apps.java
+++ src/main/java/Apps.java
@@ -1,20 +1,29 @@
 import org.apache.log4j.Logger;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
+import java.math.BigDecimal;
 import java.sql.*;
 
-public class Apps {
+public class Apps extends Thread {
     private static Logger logger=Logger.getLogger(Apps.class);
-    public static void  main(String[] args) throws SQLException {
-        String JDBCURL=args[0];
-        String SQL=args[1];
-        logger.debug("JDBCURL :"+JDBCURL);
-        logger.debug("SQL :"+ SQL);
-        Connection conn = DriverManager.getConnection(JDBCURL);
-        Statement command=conn.createStatement();
-        ResultSet dataset= command.executeQuery(SQL);
-        while (dataset.next()){
-            logger.debug(dataset.getString(1));
-        }
-        conn.close();
+    private Subscriber subscriber=new Subscriber();
+    private static String channel = "mychannel";
+    private static Jedis jedis=null;
+    public static void  main(String[] args) throws SQLException, ClassNotFoundException, IllegalAccessException, InstantiationException, InterruptedException {
+        channel=args[0];
+        JedisPoolConfig config=new JedisPoolConfig();
+        config.setTestWhileIdle(true);
+        JedisPool pool=new JedisPool(config,args[1],6379,3000,"Gepoint");
+        jedis=pool.getResource();
+        logger.debug(jedis.ping());
+        Apps apps=new Apps();
+        apps.start();
+    }
+
+    @Override
+    public void run() {
+        jedis.subscribe(subscriber, channel);
     }
 }
