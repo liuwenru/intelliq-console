303305a474efdaa231627928f7bfa3f33d8fd9b8
ijarvis <ijarvis@sina.com>
2018-01-28 23:38:18 +0800

    fix bug

--- src/main/java/Apps.java
+++ src/main/java/Apps.java
@@ -1,11 +1,8 @@
 import org.apache.log4j.Logger;
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
-import redis.clients.jedis.JedisPoolConfig;
 
 public class Apps extends Thread {
     private static Logger logger=Logger.getLogger(Apps.class);
     public static void  main(String[] args) {
-
+        logger.debug("Apps..........");
     }
 }
