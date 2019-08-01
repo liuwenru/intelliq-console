12c7dfb49e459a42b43f97add96f37d86d8a513f
ijarvis <ijarvis@sina.com>
2019-08-01 16:27:10 +0800

    fix bug

--- src/main/java/RabbitMQUtils/Apps.java
+++ src/main/java/RabbitMQUtils/Apps.java
@@ -1,19 +1,15 @@
-package rabbitmqutils;
+package RabbitMQUtils;
 
-import FileApps.EncodingDetect;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import java.io.*;
 import java.net.URISyntaxException;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
-import java.sql.SQLException;
 import java.util.concurrent.TimeoutException;
 
-import org.apache.commons.io.FileUtils;
 public class Apps {
     private final static String QUEUE_NAME="hello";
     public static void main(String[] args) throws NoSuchAlgorithmException, KeyManagementException, URISyntaxException, IOException, TimeoutException {
---
+++ src/test/java/App_Test.java
@@ -0,0 +1,22 @@
+import JacocoUtils.Calculator;
+import org.junit.Assert;
+import org.junit.Test;
+public class App_Test {
+    private Calculator instance = new Calculator();
+
+    @Test
+    public void testAdd() {
+        int a = 10;
+        int b = 20;
+        int expected = 30;
+        Assert.assertEquals(expected, instance.add(a, b));
+    }
+
+    @Test
+    public void testSub() {
+        int a = 10;
+        int b = 20;
+        int expected = -10;
+        Assert.assertEquals(expected, instance.sub(a, b));
+    }
+}
