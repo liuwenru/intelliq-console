8eb1dfef381ff3aa9232251794d9637d45511734
ijarvis <ijarvis@sina.com>
2019-09-11 15:55:32 +0800

    fix bug

---
+++ src/main/java/HDFSApps/HDFSApps.java
@@ -0,0 +1,4 @@
+package HDFSApps;
+
+public class HDFSApps {
+}
---
+++ src/main/java/RabbitMQUtils/App_withssl.java
@@ -0,0 +1,65 @@
+package RabbitMQUtils;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.GetResponse;
+import org.slf4j.Logger;
+
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.*;
+import java.security.cert.CertificateException;
+import java.util.concurrent.TimeoutException;
+
+public class App_withssl {
+
+    public static void  main(String[] args) throws KeyStoreException, IOException, CertificateException, NoSuchAlgorithmException, UnrecoverableKeyException, KeyManagementException, TimeoutException {
+        char[] keyPassphrase = "p@ssw0rd".toCharArray();
+        KeyStore ks = KeyStore.getInstance("PKCS12");
+        ks.load(new FileInputStream(args[0]), keyPassphrase);
+
+        KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+        kmf.init(ks, keyPassphrase);
+
+        char[] trustPassphrase = "p@ssw0rd".toCharArray();
+        KeyStore tks = KeyStore.getInstance("JKS");
+        tks.load(new FileInputStream(args[1]), trustPassphrase);
+
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+        tmf.init(tks);
+
+        SSLContext c = SSLContext.getInstance("TLSv1.1");
+        c.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost(args[2]);
+        factory.setPort(5671);
+        factory.useSslProtocol(c);
+        factory.setUsername("epoint");
+        factory.setPassword("epoint");
+        Connection conn = factory.newConnection();
+        Channel channel = conn.createChannel();
+
+        channel.queueDeclare("rabbitmq-java-test", false, true, true, null);
+        channel.basicPublish("", "rabbitmq-java-test", null, "Hello, World via SSL".getBytes());
+
+        GetResponse chResponse = channel.basicGet("rabbitmq-java-test", false);
+        if (chResponse == null)
+        {
+            System.out.println("No message retrieved");
+        }
+        else
+        {
+            byte[] body = chResponse.getBody();
+            System.out.println("Recieved: " + new String(body));
+        }
+
+        channel.close();
+        conn.close();
+
+    }
+}
