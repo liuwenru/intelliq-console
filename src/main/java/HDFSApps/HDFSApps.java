ca6a4c91dfc9080971d2b5509e52947ea2911bb5
ijarvis <ijarvis@sina.com>
2019-09-16 19:33:39 +0800

    fix bug

--- src/main/java/HDFSApps/HDFSApps.java
+++
@@ -1,4 +0,0 @@
-package HDFSApps;
-
-public class HDFSApps {
-}
---
+++ src/main/java/HadoopApps/HDFSApps.java
@@ -0,0 +1,34 @@
+package HadoopApps;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
+import org.apache.hadoop.security.UserGroupInformation;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+
+public class HDFSApps {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        //System.setProperty("java.security.krb5.kdc","192.168.186.126:88");
+        //System.setProperty("java.security.krb5.realm","epoint");
+        System.setProperty("java.security.krb5.conf","/home/ijarvis/workspace/javaWorkSpace/intelliq-console/src/main/resources/krb5.conf");
+        Configuration conf=new Configuration();
+        String uri="hdfs://192.168.186.124/user/dtrace_topics_java.pdf";
+        conf.set("hadoop.security.authentication", "Kerberos");
+        conf.set("fs.defaultFS","hdfs://192.168.186.124:8020");
+        conf.set("dfs.namenode.kerberos.principal.pattern", "*/*@epoint");
+        UserGroupInformation.setConfiguration(conf);
+
+        UserGroupInformation.loginUserFromKeytab("hdfs@epoint", "/home/ijarvis/workspace/javaWorkSpace/intelliq-console/src/main/resources/client_hdfs.keytab");
+        FileSystem fs=FileSystem.get(conf);
+        fs.copyFromLocalFile(new Path("/home/ijarvis/Packages/dtrace_topics_java.pdf"),new Path("/user/dtrace_topics_java.pdf"));
+        InputStream in=null;
+        in=fs.open(new Path(uri));
+        IOUtils.copyBytes(in,System.out,4096,false);
+    }
+}
+
+
