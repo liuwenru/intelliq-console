b2a0e6eec5d4d229b2be8550f903b7928a87a973
ijarvis <ijarvis@sina.com>
2019-10-24 12:38:32 +0800

    fix  bug

--- src/main/java/HadoopApps/HiveApps.java
+++ src/main/java/HadoopApps/HiveApps.java
@@ -12,8 +12,8 @@ public class HiveApps {
         Configuration conf=new Configuration();
         System.setProperty("java.security.krb5.conf","/home/ijarvis/workspace/javaWorkSpace/intelliq-console/src/main/resources/krb5.conf");
         conf.set("hadoop.security.authentication", "Kerberos");
-        conf.set("fs.defaultFS","hdfs://192.168.186.124:8020");
-        conf.set("dfs.namenode.kerberos.principal.pattern", "*/*@epoint");
+        //conf.set("fs.defaultFS","hdfs://192.168.186.124:8020");
+        //conf.set("dfs.namenode.kerberos.principal.pattern", "*/*@epoint");
         UserGroupInformation.setConfiguration(conf);
         UserGroupInformation.loginUserFromKeytab("hdfs@epoint", "/home/ijarvis/workspace/javaWorkSpace/intelliq-console/src/main/resources/client_hdfs.keytab");
         Class.forName(driverName);
