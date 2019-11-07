0cdc6dd8e49ef82f23cf58883cd5511f48d2aa92
ijarvis <ijarvis@sina.com>
2019-11-07 15:17:16 +0800

    fix bug

--- src/main/java/HadoopApps/PhoenixAppswithKerberos.java
+++ src/main/java/HadoopApps/PhoenixAppswithKerberos.java
@@ -18,6 +18,7 @@ public class PhoenixAppswithKerberos {
         config.set("zookeeper.znode.parent", "/hbase-secure");
         config.set("hbase.master.kerberos.principal", HBASE_MASTER_PRINCIPAL);
         config.set("hbase.regionserver.kerberos.principal",HBASE_RS_PRINCIPAL);
+        config.set("phoenix.queryserver.kerberos.principal", HBASE_MASTER_PRINCIPAL);
         config.set("hbase.security.authentication", "kerberos");
         config.set("hadoop.security.authentication", "Kerberos");
         config.set("hadoop.rpc.protection", "privacy");
@@ -27,18 +28,18 @@ public class PhoenixAppswithKerberos {
         Properties properties=new Properties();
         properties.setProperty("hbase.zookeeper.quorum", "epnode1.epoint,epnode2.epoint,epnode3.epoint");
         properties.setProperty("hbase.master.kerberos.principal", HBASE_MASTER_PRINCIPAL);
-        properties.setProperty("hbase.regionserver .kerberos.principal", HBASE_MASTER_PRINCIPAL);
+        properties.setProperty("hbase.regionserver.kerberos.principal", HBASE_MASTER_PRINCIPAL);
         properties.setProperty("phoenix.queryserver.kerberos.principal", HBASE_MASTER_PRINCIPAL);
         properties.setProperty("hbase.security.authentication", "kerberos");
         properties.setProperty("hadoop.security.authentication", "kerberos");
-        //properties.setProperty("hbase.myclient.keytab", "/home/ijarvis/workspace/javaWorkSpace/intelliq-console/src/main/resources/client_hdfs.keytab");
-        //Connection conn = DriverManager.getConnection("jdbc:phoenix:epnode1.epoint:2181:/hbase-secure:hdfs@epoint:/home/ijarvis/workspace/javaWorkSpace/intelliq-console/src/main/resources/client_hdfs.keytab",properties);
-        Connection conn = DriverManager.getConnection("jdbc:phoenix:epnode1.epoint:2181:/hbase-secure:hdfs@epoint:/home/ijarvis/workspace/javaWorkSpace/intelliq-console/src/main/resources/client_hdfs.keytab");
+        properties.setProperty("zookeeper.znode.parent", "/hbase-secure");
+        Connection conn = DriverManager.getConnection("jdbc:phoenix:epnode1.epoint:2181:/hbase-secure:hdfs@epoint:/home/ijarvis/workspace/javaWorkSpace/intelliq-console/src/main/resources/client_hdfs.keytab",properties);
+        //Connection conn = DriverManager.getConnection("jdbc:phoenix:epnode1.epoint:2181:/hbase-secure:hdfs@epoint:/home/ijarvis/workspace/javaWorkSpace/intelliq-console/src/main/resources/client_hdfs.keytab");
         Statement stmt = conn.createStatement();
-//        stmt.executeUpdate("create table testepoint (mykey integer not null primary key, mycolumn varchar)");
-//        stmt.executeUpdate("upsert into testepoint values (1,'Hello')");
-//        stmt.executeUpdate("upsert into testepoint values (2,'World!')");
-//        conn.commit();
+        stmt.executeUpdate("create table testepoint (mykey integer not null primary key, mycolumn varchar)");
+        stmt.executeUpdate("upsert into testepoint values (1,'Hello')");
+        stmt.executeUpdate("upsert into testepoint values (2,'World!')");
+        conn.commit();
         PreparedStatement statement = conn.prepareStatement("select * from testepoint");
         ResultSet rset = statement.executeQuery();
         while (rset.next()) {
