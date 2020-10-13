1724025c7755db458de44112fdd1e6e99ed59ba3
liuwenru <ijarvis@sina.com>
2020-10-13 13:53:40 +0800

    fix bug

--- src/main/java/Apps.java
+++ src/main/java/Apps.java
@@ -1,8 +1,8 @@
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
-import com.alibaba.druid.sql.dialect.mysql.ast.MysqlForeignKey;
 public class Apps {
     public static void main(String[] args) throws UnsupportedEncodingException {
         System.out.println(URLEncoder.encode("OA                            数据库    ","UTF-8"));
+        
     }
 }
