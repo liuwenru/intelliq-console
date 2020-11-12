ee0eb1a482d540d08ad5bbced77aab4f45a963c7
ijarvis <ijarvis@sina.com>
2020-11-12 15:30:51 +0800

    添加文件系统 RandmonAccess的写入方法

--- src/main/java/Apps.java
+++ src/main/java/Apps.java
@@ -1,6 +1,10 @@
 import java.io.UnsupportedEncodingException;
+import java.math.BigDecimal;
 import java.net.URLEncoder;
+import com.sun.jna.NativeLong;
 public class Apps {
     public static void main(String[] args) throws UnsupportedEncodingException {
+        long a = 1024*1024*1024*5L;
+        System.out.println(a);
     }
 }
