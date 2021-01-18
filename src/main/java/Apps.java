28719bef7e25e5a56e7e3e0b125fb46ac956ceac
liuwenru <ijarvis@sina.com>
2021-01-18 11:54:25 +0800

    fix bug

--- src/main/java/Apps.java
+++ src/main/java/Apps.java
@@ -1,11 +1,33 @@
 import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Method;
 import java.math.BigDecimal;
 import java.net.URLEncoder;
 import com.sun.jna.NativeLong;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Apps {
-    public static void main(String[] args) throws UnsupportedEncodingException {
-        String name=URLEncoder.encode("需求确认单-任务登记V1.0（电子版）.doc", "UTF-8");
-       System.out.println(name);
+    public static void main(String[] args) throws Exception {
+
+        System.setSecurityManager(null);
+        try {
+            Class clazz = Class.forName("Apps");
+            Object refTest = clazz.newInstance();
+
+            Method method = clazz.getDeclaredMethod("refMethod");
+
+            long now = System.currentTimeMillis();
+            for (int i=0;i<5000000;i++){
+                method.invoke(refTest);
+            }
+            System.out.println("get耗时"+(System.currentTimeMillis() - now) + "ms");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+    public void refMethod() {
+        int i=0;
+        i++;
     }
 }
