7d3177e682da9ecc3aef30a317fc50d0c0908e10
liuwenru <ijarvis@sina.com>
2021-05-22 18:22:26 +0800

    fix bug

--- src/main/java/DruidApps/Apps.java
+++ src/main/java/DruidApps/Apps.java
@@ -1,35 +1,32 @@
 package DruidApps;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Apps {
+    private  static Logger logger= LoggerFactory.getLogger(Apps.class);
     public static void main(String[] args) throws SQLException, InterruptedException {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
         DataSource dataSource = (DataSource) context.getBean("dataSource");
+
+
         Connection connection = dataSource.getConnection();
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery("select 1 from employees where employees.emp_no=11");
-
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    connection.close();
-                } catch (SQLException throwables) {
-                    throwables.printStackTrace();
-                }
-            }
-        }).start();
-        //Statement statement1=connection.createStatement();
-        //ResultSet resultSet1= statement1.executeQuery("select 1 from employees where employees.emp_no=11");
-        Thread.sleep(1000 * 4);
+        PreparedStatement preparedStatement=connection.prepareStatement("select * from sbtest6 where pad=?");
+        preparedStatement.setString(1,args[0]);
+        ResultSet resultSet= preparedStatement.executeQuery();
+        while (resultSet.next()){
+            logger.debug("  ----      %s  - %s - %s - %s  ---- ",
+                    resultSet.getString(1),
+                    resultSet.getString(2),
+                    resultSet.getString(3),
+                    resultSet.getString(4)
+            );
+        }
+        resultSet.close();
         connection.close();
-
     }
 }
