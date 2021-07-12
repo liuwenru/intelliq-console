188b5c4d55578f29cd67bb06675eea04fbeac5df
liuwenru <ijarvis@sina.com>
2021-07-12 14:07:35 +0800

    fix bug

--- src/main/java/Apps.java
+++ src/main/java/Apps.java
@@ -4,6 +4,12 @@ public class Apps {
     public static void main(String[] args) throws Exception {
 //        java.nio.channels.Selector.open().select();
         String random=RandomStringUtils.random(11,"1234567890");
-        System.out.println(random);
+
+        Package[] packages=Package.getPackages();
+
+        for (int i=0;i<packages.length;i++){
+            System.out.println(packages[i].toString());
+
+        }
     }
 }
